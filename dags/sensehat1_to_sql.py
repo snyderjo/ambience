d30238a7557d2b419258d67dec73a28cfea875fe
John Snyder <snyderjo@gmail.com>
2026-02-01 15:03:26 -0800

    updated SENSEHAT1_TO_SQL.PY dag to match that of sensehat2, i.e. pulling reference from the database, and changed the location referenced.

--- dags/sensehat1_to_sql.py
+++ dags/sensehat1_to_sql.py
@@ -1,26 +1,59 @@
+from sqlalchemy import create_engine, text
+from credentials import Username, password
 from airflow import DAG, macros
-#from airflow.macros import ds_add, ds_format
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from datetime import timedelta,datetime
+import pendulum
 import os
+
+#query the necessary metadata
+ROOM = "office"
+OUTPUT_DIR = "/home/john/Documents/projects/ambience/sensehatData2/"
+
+# create a URL string
+
+# url_str = URL.create(
+#     "postgresql+psycopg2"
+#     ,host = "woadamsj-All-Series"
+#     ,username = Username
+#     ,password = password
+#     ,database = "johnsdb"
+# )
+
+# create a database engine
+query_string = text(
+    """
+    select a.id, a.timezone, a.startrecorddt, b.hostname, b.username, b.pathname
+    from ambience.location a
+    left join ambience.pi b on a.pi_id = b.id
+    where 1 = 1
+        and a.active
+        and a.room = :roomName
+    """
+    )
+
+engine = create_engine(f"postgresql+psycopg2://{Username}:{password}@woadamsj-All-Series:5432/johnsdb")
+
+with engine.connect() as conn:
+    result = conn.execute(query_string,{"roomName":ROOM})
+    pihat_loc_id, pihat_timezone, pihat_start_dt, pihat_hostname, pihat_username, pihat_path  =  result.fetchone()
+
+
+local_tz = pendulum.timezone(pihat_timezone)
+DAG_start = datetime(pihat_start_dt.year,pihat_start_dt.month,pihat_start_dt.day, tzinfo=local_tz)
 
 #grab the previous day's readings
 
-output_dir = "/home/john/Documents/projects/ambience/sensehatData1/"
-pihat_host_name = 'raspberrypihat'
-pi_hat_username = 'pi'
-pihat_path = '/home/pi/Documents/projects/ambience/output/'
-
-def csvToPostgres(path=output_dir,file=""):
+def csvToPostgres(path=OUTPUT_DIR,file=""):
     #Open Postgres Connection
     pg_hook = PostgresHook(postgres_conn_id='ambience')
     #get_postgres_conn = PostgresHook(postgres_conn_id='ambience').get_conn()
     pg_hook.copy_expert(
         sql="""
-        COPY ambience.readings_stage (reading_dttm,temp,pressure,humidity,pitch,roll,yaw,accel_x,accel_y,accel_z) 
+        COPY ambience.readings_stage2 (reading_dttm,temp,pressure,humidity,pitch,roll,yaw,accel_x,accel_y,accel_z)
         from STDIN WITH CSV DELIMITER as ','
         """
         ,filename=path+file
@@ -34,16 +67,17 @@ default_args = {
 
 
 with DAG(
-    dag_id = 'sensehat1_to_sql'
-    ,description = 'this dag moves data from sense hat id 1 to postgres database'
-    ,start_date=datetime(2022,12,10)
+    dag_id = 'sensehat2_to_sql'
+    ,description = 'this dag moves data from sense hat id 2 to postgres database'
+    ,start_date=DAG_start
     ,catchup = True
-    ,schedule_interval = '00 3 * * *'
+    ,schedule_interval = '10 3 * * *'
     ,params={
-        'PI_USER':pi_hat_username
-        ,'PI_HOST':pihat_host_name
+        'PI_USER':pihat_username
+        ,'PI_HOST':pihat_hostname
         ,'PI_PATH': pihat_path
-        ,"LOC_OUT_DIR": output_dir
+        ,'LOC_OUT_DIR': OUTPUT_DIR
+        ,'LOC_ID': pihat_loc_id
         }
 ) as dag:
     
@@ -53,7 +87,7 @@ with DAG(
 
     copy_task = BashOperator(
         task_id = 'scp_cmd'
-        ,bash_command = "scp {{ params.PI_USER }}@{{ params.PI_HOST }}:{{ params.PI_PATH }}readings_$filedate.csv {{ params.LOC_OUT_DIR}}"
+        ,bash_command = "scp {{ params.PI_USER }}@{{ params.PI_HOST }}:{{ params.PI_PATH }}/readings_$filedate.csv {{ params.LOC_OUT_DIR}}"
         ,env = {"filedate": '{{ macros.ds_format(macros.ds_add(ds,-1),"%Y-%m-%d","%Y_%m_%d") }}'}
     )
 
@@ -90,15 +124,16 @@ with DAG(
                 accel_x,
                 accel_y,
                 accel_z,
-                1
-            from ambience.readings_stage;
+                %(val)s
+            from ambience.readings_stage2;
             """
+           ,parameters={"val": pihat_loc_id }
     )
 
     clear_stage = PostgresOperator(
         task_id = 'clear_stage'
         ,postgres_conn_id = 'ambience'
-        ,sql = 'delete from ambience.readings_stage where 1 = 1;'
+        ,sql = 'delete from ambience.readings_stage2 where 1 = 1;'
     )
 
     zip_csv = BashOperator(
